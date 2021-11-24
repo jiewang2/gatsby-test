9e95729ead2418d7efe625d82e881bb8c5ccf551
wangjie-agora <wangjie@agora.io>
2021-11-24 17:55:19 +0800

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://gatsbytestmain97458.gatsbyjs.io/",
-    title: "Agora Doc",
+    title: "Gatsby Demo",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
